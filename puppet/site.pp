19c47899de82a3f500d9ad47ddf36cf8c0180afc
fatihsucu <fatihsucu0@gmail.com>
2016-03-23 15:03:47 +0200

    test3

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,11 +1,6 @@
 package {
         "build-essential": ensure => latest;
-        "python": ensure => "2.7.5-5ubuntu3";
-        "apache2-utils": ensure => "installed";
-        "python-dev": ensure => "2.7.5-5ubuntu3";
-        "python-setuptools": ensure => '3.3-1ubuntu2';
         "vim": name=> 'vim', ensure=>'installed';
-        "mongodb-10gen":ensure => "present";
         "pip": name=>'python-pip', ensure=> 'latest';
         "pymongo": ensure=> '3.2', provider=> 'pip',require=> Package['pip'];
         }
